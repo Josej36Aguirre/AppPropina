b0392d7379b63c9918c75826a5d0f01c49ada530
Josej36Aguirre <josej36.aguirre@gmail.com>
2018-05-16 21:38:49 -0500

    App Propina

---
+++ AppPropina/AppPropina/Model/Propina.cs
@@ -0,0 +1,131 @@
+﻿
+
+namespace AppPropina.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using ViewModel;
+    public class Propina: Notificable
+    {
+        #region atributos
+        private double total;
+        private int personas;
+        private double propinaTotal;
+        private double totalPersonas;
+        private double propinaPorPersona;
+        private double porcentajePropina;
+        private double totalCuenta;
+        #endregion
+        #region Propiedades
+        public double TotalCuenta
+        {
+            get { return totalCuenta; }
+            set
+            {
+                if (totalCuenta == value)
+                {
+                    return;
+                }
+                totalCuenta = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public double PorcentajePropina
+        {
+            get { return porcentajePropina; }
+            set
+            {
+                if (porcentajePropina == value)
+                {
+                    return;
+                }
+                porcentajePropina = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public double PropinaPorPersona
+        {
+            get { return propinaPorPersona; }
+            set
+            {
+                if (propinaPorPersona == value)
+                {
+                    return;
+                }
+                propinaPorPersona = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public double TotalPersonas
+        {
+            get { return totalPersonas; }
+            set
+            {
+                if (total == value)
+                {
+                    return;
+                }
+                totalPersonas = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public double PropinaTotal
+        {
+            get { return propinaTotal; }
+            set
+            {
+                if (propinaTotal == value)
+                {
+                    return;
+                }
+                propinaTotal = value;
+                OnPropertyChanged();
+            }
+
+        }
+
+
+        public int Personas
+        {
+            get { return personas; }
+            set
+            {
+                if (personas == value)
+                {
+                    return;
+                }
+                personas = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Total
+        {
+            get { return total; }
+            set
+            {
+                if (total == value)
+                {
+                    return;
+                }
+                total = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+
+
+
+
+    }
+}
---
+++ AppPropina/AppPropina/ViewModel/PropinaViewModel.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace AppPropina.ViewModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Model;
+    using Xamarin.Forms;
+
+    public   class PropinaViewModel
+    {
+        #region propiedades
+
+        public Propina PropinaModel { get; set; }
+        public Command CalcularCommand { get; set; }
+        #endregion
+
+        public PropinaViewModel()
+        {
+            PropinaModel = new Propina();
+            CalcularCommand = new Command(Calcular);
+        }
+
+        private void Calcular(object obj)
+        {
+
+    
+
+            PropinaModel.PropinaPorPersona = PropinaModel.PropinaTotal / PropinaModel.Personas;
+            PropinaModel.TotalPersonas = PropinaModel.Total / PropinaModel.Personas;
+            PropinaModel.PropinaTotal = (PropinaModel.Total * PropinaModel.PorcentajePropina) / 100;
+            PropinaModel.TotalCuenta = PropinaModel.Total + PropinaModel.Total;
+
+          
+        }
+    }
+}
